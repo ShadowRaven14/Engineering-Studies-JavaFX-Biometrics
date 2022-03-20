3654cf5d7d09f180bd7c54e02aa2f868d9f09db1
Łukasz Terpiłowski <lukaszterpilowski9@gmail.com>
2022-03-20 11:38:28 +0100

    Add FileHandler class. Save, load, convert buffered img to img methods

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,6 +5,7 @@ module com.example.biometriajavafx {
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
     requires org.kordamp.bootstrapfx.core;
+    requires java.desktop;
 
     opens com.example.biometriajavafx to javafx.fxml;
     exports com.example.biometriajavafx;
