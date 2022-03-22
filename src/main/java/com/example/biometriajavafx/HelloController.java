82ae7c3921b397f00ec4bd6b06a2fba42dc548de
Łukasz Terpiłowski <lukaszterpilowski9@gmail.com>
2022-03-22 19:11:06 +0100

    First elements of the interface

--- src/main/java/com/example/biometriajavafx/HelloController.java
+++ src/main/java/com/example/biometriajavafx/HelloController.java
@@ -1,14 +1,41 @@
 package com.example.biometriajavafx;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
+
+import java.awt.image.BufferedImage;
 
 public class HelloController {
+
+    @FXML
+    private ImageView originalImageView;
+
     @FXML
     private Label welcomeText;
 
     @FXML
+    private Button loadImageBtn;
+
+    @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    @FXML
+    protected void onLoadImageBtnClick() {
+        Stage thisStage = (Stage) loadImageBtn.getScene().getWindow();
+
+        BufferedImage originalImage;
+        originalImage = FileHandler.LoadImage(thisStage);
+
+        //originalImageView = new ImageView();
+        originalImageView.setImage(FileHandler.convertToFxImage(originalImage));
+
+
+
+        welcomeText.setText("Load image!");
     }
 }
